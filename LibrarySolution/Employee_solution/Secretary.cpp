5341dedfd1e2f4e6d61baa28b0d88e424f0a56b1
Xue <155470055+yuanyuan233333@users.noreply.github.com>
2025-10-27 11:12:33 +0100

    Delete LibrarySolution/Employee_solution directory

--- LibrarySolution/Employee_solution/Secretary.cpp
+++
@@ -1,8 +0,0 @@
-// Secretary.cpp
-// Implementation of Secretary class
-
-#include "Secretary.hpp"
-
-Secretary::Secretary(std::string n, std::string s, unsigned int ID)
-        : Employee(n, s, ID) {}
-
